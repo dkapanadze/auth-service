abea52315f2efd1c7999e08f04837fed01e92e1d
dimkapanadze@gmail.com <dimitri.kapanadze@makingscience.com>
2024-05-29 16:39:21 +0200

    test

--- test/setup.ts
+++ test/setup.ts
@@ -1,20 +1,20 @@
-import { TokenEntity, UserEntity } from '../src/users/entities';
-import { DataSource } from 'typeorm';
+// import { TokenEntity, UserEntity } from '../src/users/entities';
+// import { DataSource } from 'typeorm';
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'myproject_test',
-  synchronize: false, // We'll handle synchronization manually
-  entities: [UserEntity, TokenEntity],
-});
+// export const AppDataSource = new DataSource({
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'postgres',
+//   database: 'myproject_test',
+//   synchronize: false, // We'll handle synchronization manually
+//   entities: [UserEntity, TokenEntity],
+// });
 
-global.afterEach(async () => {
-  await AppDataSource.initialize();
-  await AppDataSource.dropDatabase();
-  await AppDataSource.synchronize(); // Optional: to recreate the schema
-  await AppDataSource.destroy();
-});
+// global.afterEach(async () => {
+//   await AppDataSource.initialize();
+//   await AppDataSource.dropDatabase();
+//   await AppDataSource.synchronize(); // Optional: to recreate the schema
+//   await AppDataSource.destroy();
+// });
