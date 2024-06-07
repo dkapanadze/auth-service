57e1ece0f3ee9990057eeeb0360e6ddf9f041789
dimkapanadze@gmail.com <dimitri.kapanadze@makingscience.com>
2024-06-07 16:53:52 +0200

    Test

--- after_install.sh
+++ after_install.sh
@@ -6,6 +6,18 @@ echo "Script directory: $(dirname "$0")"
 
 # Set executable permission for the script itself
 chmod +x "$0"
+
+# Get the deployment directory from the environment variables
+DEPLOYMENT_DIR="$DEPLOYMENT_GROUP_NAME/$DEPLOYMENT_ID/deployment-archive"
+
+# Navigate to the deployment directory
+cd "/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_DIR"
+
+# Set executable permission for the script
+chmod +x after_install.sh
+
+# Verify the permissions after setting them
+ls -l after_install.sh
 # Replace "artifact.zip" with the actual name of your artifact zip file
 ARTIFACT="s3://my-nest-project-44444/test/buildProject.zip"
 APP_DIR="/home/ubuntu/nestsj-api"
@@ -18,7 +30,7 @@ aws s3 cp s3://my-nest-project-44444/nestjs-key.pem /tmp --recursive
 
 chmod 400 /tmp/nestjs-key.pem
 
-ssh -i  /tmp/nestjs-key.pem  -t ubuntu@ec2-54-205-124-72.compute-1.amazonaws.com << EOF
+ssh -i  /tmp/nestjs-key.pem -t -t ubuntu@ec2-54-205-124-72.compute-1.amazonaws.com << EOF
   # Navigate to the application directory
   cd $APP_DIR
 
